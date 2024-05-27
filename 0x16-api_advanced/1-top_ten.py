b67341924729e0f5c76a9fac0495d07e2491e0f4
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-05-27 06:07:20 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ import requests
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts of a subreddit."""
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {'User-Agent': 'python:subreddit.top.ten:v1.0 (by /u/yourusername)'}
+    headers = {'User-Agent': 'python:subreddit.top.ten:v1.0 (by /u/Ethar7)'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
