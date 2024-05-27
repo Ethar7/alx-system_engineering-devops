6c88551415b661d3e6ce0787e32139c34aa0dc55
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-05-27 05:59:44 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Return 'OK' if the API call is successful, otherwise return 0."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        return "OK"
-    elif response.status_code == 404:
-        return 0
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
-        return "ERROR"
+        return 0
