ebdaaee12c54125b69990fcc32ed1d7a97b9c09b
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-05-27 06:10:45 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,19 +6,20 @@ Script to print hot posts on a given Reddit subreddit.
 
 import requests
 
+
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts of a subreddit."""
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {'User-Agent': 'python:subreddit.top.ten:v1.0 (by /u/Ethar7)'}
+    headers = {'User-Agent': 'python:subreddit.top.ten:v1.0 (by /u/yourusername)'}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()  # Raise an exception if status code is not 200
+        data = response.json()
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    except requests.RequestException:
         print(None)
-        return
-
-    data = response.json()
-    for post in data['data']['children']:
-        print(post['data']['title'])
 
 if __name__ == '__main__':
     import sys
