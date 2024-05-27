ecae9c11f8c46a5831980bfc98ebf4fc5aa61862
Ethar7 <nabihethar@yahoo.com>
2024-05-26 19:54:17 -0700

    o

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,15 +2,16 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """Return 'OK' if the API call is successful, otherwise return 0."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code == 200:
+        return "OK"
+    elif response.status_code == 404:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    else:
+        return "ERROR"
