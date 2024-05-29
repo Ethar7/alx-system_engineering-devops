aab8952e75b6ee4592f80954d14dbc1d0a30724e
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-05-29 10:45:08 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+0-main
 """
-
 import requests
 
-
-import requests
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User Agent'}  # Set a custom User-Agent to avoid errors
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """returns the number of subscribers for a given subreddit"""
 
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    subreddit_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    geting_inf = requests.get(subreddit_url, headers=headers)
+
+    if geting_inf.status_code == 404:
         return 0
 
-# Test the function
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print(number_of_subscribers(sys.argv[1]))
+    data = geting_inf.json()
+    subscribe = data.get('data').get('subscribers')
+    return subscribe
